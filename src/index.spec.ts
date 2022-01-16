19a82e199196cab5e11b18b72f68aabe77b33aa8
Bopzor <violaine.ballandin@gmail.com>
2022-01-16 17:21:08 +0100

    with colors

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,69 @@
 import { expect } from 'chai';
-import { getColorFormIter } from './index';
+import { Color, getColorFormProgression, normalize, toComponent } from './index';
+
+const palette: Record<number, Color> = {
+  0: [0, 0, 0],
+  0.5: [0, 255, 0],
+  1: [0, 0, 255],
+};
+
+const interpolateColor = (color1: Color, color2: Color, factor = 0.5) => {
+  const result = { ...color1 };
+
+  for (let i = 0; i < color1.length; i++) {
+    result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+  }
+
+  return result;
+};
 
 describe('get color', () => {
-  let maxIter = 50;
-
   it('gives clearest color at each iteration', () => {
-    expect(getColorFormIter(1, maxIter)).to.eql([0, 10, 0]);
-    expect(getColorFormIter(25, maxIter)).to.eql([0, 255, 0]);
-    expect(getColorFormIter(50, maxIter)).to.eql([0, 0, 255]);
+    // expect(getColorFormProgression(0, palette)).to.eql([0, 0, 0]);
+    expect(getColorFormProgression(0.25, palette)).to.eql([0, 128, 0]);
+    // expect(getColorFormProgression(0.5, palette)).to.eql([0, 255, 0]);
+    // expect(getColorFormProgression(1, palette)).to.eql([0, 0, 255]);
+  });
+});
+
+describe('normalize', () => {
+  it('', () => {
+    expect(normalize({ value: 0.5, range: [0, 1] })).to.equal(0.5);
+  });
+
+  it('', () => {
+    expect(normalize({ value: 0.2, range: [0, 0.4] })).to.equal(0.5);
+  });
+
+  it('', () => {
+    expect(normalize({ value: 0.2, range: [0, 0.8] })).to.equal(0.25);
+  });
+
+  it('', () => {
+    expect(normalize({ value: 0.6, range: [0.4, 0.8] })).to.be.closeTo(0.5, 0.000001);
+  });
+});
+
+describe.only('toComposant', () => {
+  it('', () => {
+    expect(toComponent(0.5)).to.equal(128);
+  });
+
+  it('', () => {
+    expect(toComponent(0)).to.equal(0);
+  });
+
+  it('', () => {
+    expect(toComponent(1)).to.equal(255);
+  });
+
+  it('', () => {
+    expect(toComponent(1, 0, 0)).to.equal(0);
+  });
+
+  it('', () => {
+    expect(toComponent(1, 255, 0)).to.equal(0);
+    expect(toComponent(0.5, 255, 0)).to.equal(128);
+    expect(toComponent(0, 255, 0)).to.equal(255);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-type ComplexNumber = [number, number];
-type Pixel = [number, number];
-type Point = [number, number];
-type Color = [number, number, number];
+type Pixel = { x: number; y: number };
+type Point = { re: number; im: number };
+export type Color = [number, number, number];
 
 class Canvas {
   width = 0;
   height = 0;
   ctx: CanvasRenderingContext2D | null = null;
-
-  currentPixel: Pixel = [0, 0];
-  zoom = 200;
 
   constructor() {
     const canvas = document.querySelector('canvas');
@@ -21,11 +17,7 @@ class Canvas {
     }
   }
 
-  pixelToPoint([x, y]: Pixel): Point {
-    return [this.translateX(x, 2 / 3) / this.zoom, this.translateY(y) / this.zoom];
-  }
-
-  draw([x, y]: Pixel, color: Color) {
+  draw({ x, y }: Pixel, color: Color) {
     this.ctx!.fillStyle = this.getColorFromComponents(color);
     this.ctx!.fillRect(x, y, 1, 1);
   }
@@ -34,22 +26,77 @@ class Canvas {
     return `rgb(${r}, ${g}, ${b})`;
   }
 
-  private translateX(x: number, distance = 1 / 2) {
-    return x - this.width * distance;
+  centerX(x: number) {
+    return x - this.width / 2;
   }
 
-  private translateY(y: number, distance = 1 / 2) {
-    return y - this.height * distance;
+  centerY(y: number) {
+    return y - this.height / 2;
+  }
+}
+
+class Plan {
+  x = 0.6;
+  y = 0;
+
+  zoom = 200;
+
+  pixelToPoint({ x, y }: Pixel): Point {
+    // prettier-ignore
+    return {
+      re: x / this.zoom - this.x,
+      im: y / this.zoom - this.y,
+    };
   }
 }
 
 const canvas = new Canvas();
+const plan = new Plan();
 
-const mandelbrot = ([za, zb]: ComplexNumber, [ca, cb]: ComplexNumber): ComplexNumber => {
-  const r: number = Math.pow(za, 2) - Math.pow(zb, 2) + ca;
-  const i: number = 2 * za * zb + cb;
+const defaultPalette: Record<number, Color> = {
+  0: [255, 0, 0],
+  0.5: [0, 255, 0],
+  1: [0, 0, 255],
+};
 
-  return [r, i];
+export const normalize = ({ value, range }: { value: number; range: [number, number] }) => {
+  if (range[1] - range[0] === 0) {
+    return 0;
+  }
+
+  return (value - range[0]) / (range[1] - range[0]);
+};
+
+export const toComponent = (value: number, min = 0, max = 255): number => {
+  if (min > max) {
+    return min - Math.floor(value * (min - max));
+  }
+
+  return Math.round(value * (max - min));
+};
+
+const interpolateColor = ([r1, g1, b1]: Color, [r2, g2, b2]: Color, value = 0.5): Color => {
+  // prettier-ignore
+  return [
+    toComponent(value, r1, r2),
+    toComponent(value, g1, g2),
+    toComponent(value, b1, b2),
+  ];
+};
+
+export const getColorFormProgression = (progression: number, palette = defaultPalette): Color => {
+  const ranges: number[] = Object.keys(palette).map(Number).sort();
+
+  const idx = ranges.findIndex((range) => progression <= range);
+  const startIdx = idx - 1 < 0 ? ranges.length - 1 : idx - 1;
+  const endIdx = idx < 0 ? 0 : idx;
+
+  const startColor = palette[ranges[startIdx]];
+  const endColor = palette[ranges[endIdx]];
+
+  const value = normalize({ value: progression, range: [ranges[startIdx], ranges[endIdx]] });
+
+  return interpolateColor(startColor, endColor, value);
 };
 
 export const getColorFormIter = (iter: number, maxIter: number): Color => {
@@ -58,13 +105,6 @@ export const getColorFormIter = (iter: number, maxIter: number): Color => {
   let r = 0;
   let g = 0;
   let b = 0;
-
-  // if (coef < 0.5) {
-  //   g = Math.floor(coef * 2 * 255);
-  // } else {
-  //   g = 255 - Math.floor((coef - 0.5) * 2 * 255);
-  //   b = Math.floor((coef - 0.5) * 2 * 255);
-  // }
 
   if (coef < 1 / 3) {
     g = Math.floor(coef * 3 * 255);
@@ -79,29 +119,49 @@ export const getColorFormIter = (iter: number, maxIter: number): Color => {
   return [r, g, b];
 };
 
-const main = (maxIter = 10) => {
+const mandelbrot = (maxIter = 10) => {
   for (let x = 0; x < canvas.width; x++) {
     for (let y = 0; y < canvas.height; y++) {
-      const c = canvas.pixelToPoint([x, y]);
+      const c = plan.pixelToPoint({ x: canvas.centerX(x), y: canvas.centerY(y) });
 
-      let z: ComplexNumber = [0, 0];
+      let z: Point = { re: 0, im: 0 };
 
       let color: Color = [0, 0, 0];
 
       for (let iter = 1; iter <= maxIter; iter++) {
-        const [za, zb] = mandelbrot(z, c);
+        const zr: number = z.re * z.re - z.im * z.im + c.re;
+        const zi: number = 2 * z.re * z.im + c.im;
 
-        if (Math.pow(za, 2) + Math.pow(zb, 2) >= 4) {
-          color = getColorFormIter(iter, maxIter);
+        if (zr * zr + zi * zi >= 4) {
+          color = getColorFormProgression(iter / maxIter);
           break;
         }
 
-        z = [za, zb];
+        z = { re: zr, im: zi };
       }
 
-      canvas.draw([x, y], color);
+      canvas.draw({ x, y }, color);
     }
   }
 };
 
-main(250);
+const actions: { [key: KeyboardEvent['key']]: (() => void) | undefined } = {
+  ArrowRight: () => (plan.x -= 30 / plan.zoom),
+  ArrowLeft: () => (plan.x += 30 / plan.zoom),
+  ArrowUp: () => (plan.y += 30 / plan.zoom),
+  ArrowDown: () => (plan.y -= 30 / plan.zoom),
+  ' ': () => (plan.zoom *= 1.5),
+  Alt: () => (plan.zoom /= 1.5),
+};
+
+const main = (maxIter = 10) => {
+  window.addEventListener('keydown', (event: KeyboardEvent) => {
+    actions[event.key]?.();
+
+    mandelbrot(maxIter);
+  });
+
+  mandelbrot(maxIter);
+};
+
+main(50);
